ec7cb15bc1ad9e81191176cfe6a05521bbd666f9
Carlos <undefined751@gmail.com>
2016-03-04 01:20:17 -0300

    testing something in production

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,7 +2,7 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   # TODO - fix this, cant logout this way.
-  #protect_from_forgery with: :exception
+  protect_from_forgery with: :exception
 
 
   include SessionsHelper
